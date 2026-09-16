93a9e90a647bee461b239e08f1f0f510da8222e3
Lunobe <257240031+Lunobe@users.noreply.github.com>
2026-09-16 06:26:20 +0300

    nx: auto-commit before deploy

--- configuration.nix
+++ configuration.nix
@@ -205,6 +205,8 @@
 
   programs.steam.enable = true;
 
+  programs.gamescope.enable = true;
+
   programs.ssh.knownHosts.github-com = {
     hostNames = ["github.com"];
     publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
